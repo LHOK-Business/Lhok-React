cc28c3a5f6a0cb39aae069d588802b435ff41b4e
LHOK-Business <hunain.jd@gmail.com>
2026-06-30 14:46:50 -0400

    Create breakpoints.js

---
+++ src/styles/breakpoints.js
@@ -0,0 +1,8 @@
+// standard breakpoints for websites
+
+export const breakpoints = {
+  sm: 480,   // large phones
+  md: 768,   // tablets
+  lg: 1024,  // small desktops / landscape tablets
+  xl: 1280,  // desktop
+};
